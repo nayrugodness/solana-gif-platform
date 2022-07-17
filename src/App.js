4cbacb070b1965b2b245e309633b62773a04a5a2
Nayru <nanualexandraramirez@gmail.com>
2022-07-17 18:24:20 -0500

    holding state

--- src/App.js
+++ src/App.js
@@ -61,14 +61,15 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      {/* This was solely added for some styling fanciness */}
+			<div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {/* Render your connect to wallet button right here */}
-          {renderNotConnectedContainer()}
+          {/* Add the condition to show this only if we don't have a wallet address */}
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
